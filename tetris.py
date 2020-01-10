e6389d9c4dca1c9b8bec4261fad3f19968d6b3e5
Mubelotix <mubelotix@gmail.com>
2020-01-10 13:39:58 +0100

    Initial commit

---
+++ tetris.py
@@ -0,0 +1,29 @@
+def display_slab(window, slab_color_number, x, y):
+    """
+    Draw a single square on the window. Coords 0;0 is the square to the top left.
+    """
+    pass
+
+def display_grid(window, grid):
+    """
+    Draw every squares of the grid to the window.
+    """
+    # Utiliser la fonction display_slab()
+    pass
+
+def can_squares_move(grid, squares, direction):
+    """
+    Take the grid, an array of squares and a direction number.
+    Direction is number 1 for down, 2 for for left and 3 for right.
+    Return true if every square is able to move in the direction.
+    """
+    pass
+
+def move_squares(grid, squares, direction):
+    """
+    Take the grid, an array of squares and a direction number.
+    Direction is number 1 for down, 2 for for left and 3 for right.
+    Squares will be moved in the array, the actualised array will be returned.
+    THE MOVE MUST BE POSSIBLE, NO CHECK IN THIS FUNCTION.
+    """
+    pass
